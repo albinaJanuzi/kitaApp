6473053e2d0d1cc094c72d091b80f820b96b90bc
Albina Januzi <albina_januzi@hotmail.com>
2026-02-06 23:29:07 +0100

    Add news Model

---
+++ kindergarten-app/src/app/models/news.model.ts
@@ -0,0 +1,6 @@
+export interface News{
+    id?: string;
+    title: string;
+    text: string;
+    date: Date;
+}
--- kindergarten-app/src/app/news/news.ts
+++ kindergarten-app/src/app/news/news.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Firestore, collection, addDoc, collectionData } from '@angular/fire/firestore';
 import { CommonModule } from '@angular/common';
+import { News } from '../models/news.model';
 
 @Component({
   selector: 'app-news',
@@ -11,22 +12,25 @@ import { CommonModule } from '@angular/common';
 })
 export class NewsComponent {
   
-  newsList: any[] = [];
+  newsList: News[] = [];
 
   constructor(private firestore: Firestore) {
     const ref = collection(this.firestore, 'news');
     collectionData(ref).subscribe(data => {
-      this.newsList = data;
+      this.newsList = data as News[];
     });
   }
 
   addNews(title: string, text: string) {
     const ref = collection(this.firestore, 'news');
-    addDoc(ref, {
-      title: title,
-      text: text,
-      date: new Date() 
-    });
+    
+    const news: News = {
+      title,
+      text,
+      date: new Date()
+    };
+
+    addDoc(ref, news);
   }
 
 }
