3f403a980ceba5b4bd267626c2bd715b11f43a31
Albina Januzi <albina_januzi@hotmail.com>
2026-02-11 15:32:56 +0100

    Auth Guard (Allow User to Navigate in News Only when LoggedIn)

--- kindergarten-app/src/app/app.routes.ts
+++ kindergarten-app/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login';
 import { NewsComponent } from './news/news';
+import { authGuard } from './auth/auth-guard';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/login', pathMatch: 'full' },
-    { path: 'login', component: LoginComponent },
-    { path: 'news', component: NewsComponent }
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'news', component: NewsComponent, canActivate: [authGuard] }
 ];
