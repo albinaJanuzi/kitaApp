905fcbe47a031ec944e0a4a4e5b8cdd6722b669d
Albina Januzi <albina_januzi@hotmail.com>
2026-02-11 15:52:07 +0100

    Show User Email, when LoggedIn

--- kindergarten-app/src/app/shared/navbar/navbar.ts
+++ kindergarten-app/src/app/shared/navbar/navbar.ts
@@ -1,20 +1,31 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { Auth, signOut } from '@angular/fire/auth';
+import { authState } from '@angular/fire/auth';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './navbar.html'
 })
 export class NavbarComponent {
 
-  constructor(private auth: Auth, private router: Router) {}
+  userEmail: string | null = null;
+
+  constructor(private auth: Auth, private router: Router) {
+  authState(this.auth).subscribe(user => {
+    this.userEmail = user?.email ?? null;
+  });
+}
+
 
   logout() {
     signOut(this.auth).then(() => {
       this.router.navigate(['/login']);
     });
   }
+
+  
 }
